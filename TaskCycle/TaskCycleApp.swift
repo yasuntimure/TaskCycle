3304bd1c83c126f3d6f963d6e32dc258b12db341
Eyup Yasuntimur <yasuntimure@gmail.com>
2023-08-04 11:41:00 -0700

    Initial Commit

---
+++ TaskCycle/TaskCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCycleApp.swift
+//  TaskCycle
+//
+//  Created by Eyüp on 2023-08-04.
+//
+
+import SwiftUI
+
+@main
+struct TaskCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
