2022ef87f01ae7fff3edfe007074dbacf2937a69
Eyup Yasuntimur <yasuntimure@gmail.com>
2023-08-13 15:27:55 -0700

    primary button UI updated

--- TaskCycle/UI/Components/Views/PrimaryButton.swift
+++ TaskCycle/UI/Components/Views/PrimaryButton.swift
@@ -9,43 +9,30 @@ import SwiftUI
 
 struct PrimaryButton: View {
 
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
-    
     @State var title: String
     @State var width: CGFloat = ScreenSize.defaultWidth
     @State var height: CGFloat = 50
     @State var action: () -> Void
 
+    var cornerRadius: CGFloat = 20
     
     var body: some View {
         Button(action: action) {
-
             Text(title)
-                .foregroundColor(colorScheme == .light ? .black : .white)
-                .font(.system(size: 20))
-                .bold()
                 .frame(width: width, height: height)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(Color.primary, lineWidth: 2)
-                )
-
-
+                .foregroundColor(.white)
+                .font(.system(size: 20)).bold()
+                .background(Color.mTintColor)
         }
-        .padding(.top)
-
+        .cornerRadius(cornerRadius)
     }
 
 }
 
 struct PrimaryButton_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            Color.blue
-
-            PrimaryButton(title: "Login") {
-                // action
-            }
+        PrimaryButton(title: "Login") {
+            // action
         }
     }
 }
